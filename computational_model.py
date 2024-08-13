11ee96dc88dcad9a528405b5442bc32f7cdc8c6f
Jeremy Kun <j2kun@users.noreply.github.com>
2024-08-13 13:49:59 -0700

    add model and naive matrix packing

---
+++ computational_model.py
@@ -0,0 +1,40 @@
+"""The arithmetic/SIMD model of FHE."""
+
+
+class Ciphertext:
+    def __init__(self, data: list[int]):
+        self.data = data
+        self.dim = len(data)
+
+    def __add__(self, other: "Ciphertext") -> "Ciphertext":
+        assert self.dim == other.dim
+        return Ciphertext([self.data[i] + other.data[i] for i in range(len(self.data))])
+
+    def __mul__(self, other: "Ciphertext") -> "Ciphertext":
+        assert self.dim == other.dim
+        return Ciphertext([self.data[i] * other.data[i] for i in range(len(self.data))])
+
+    def rotate(self, n: int) -> "Ciphertext":
+        n = n % self.dim
+        return self.data[-n:] + self.data[:-n]
+
+
+def is_power_of_two(n: int) -> bool:
+    """Check if n is a power of two."""
+    return n & (n - 1) == 0
+
+
+def rotate_and_sum(ciphertext: Ciphertext) -> Ciphertext:
+    """Return a ciphertext where each entry contains the sum of all entries in
+    the input ciphertext."""
+    n = len(ciphertext.data)
+    assert is_power_of_two(n)
+    # copy so as not to mutate the input
+    result = Ciphertext(ciphertext.data[:])
+
+    shift = n // 2
+    while shift > 0:
+        result += result.rotate(shift)
+        shift //= 2
+
+    return result
---
+++ halevi_shoup.py
@@ -0,0 +1,55 @@
+"""Halevi-Shoup matrix packing technique."""
+
+from computational_model import Ciphertext
+from computational_model import rotate_and_sum
+
+
+def pack_naive(matrix: list[list[int]]) -> list[Ciphertext]:
+    """Naively pack the matrix into a list of ciphertexts."""
+    assert len(matrix) == len(matrix[0])
+    n = len(matrix)
+    return [Ciphertext(matrix[i]) for i in range(n)]
+
+
+def pack(matrix: list[list[int]]) -> list[Ciphertext]:
+    """Pack the matrix into a list of ciphertexts via Halevi-Shoup."""
+    assert len(matrix) == len(matrix[0])
+
+    n = len(matrix)
+    ciphertexts = [[None] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            ciphertexts[i][j] = matrix[j][(i+j) % n]
+
+    return [Ciphertext(ciphertexts[i]) for i in range(n)]
+
+
+def multiply_naive(packed_matrix: list[Ciphertext], vector: Ciphertext) -> Ciphertext:
+    """Multiply the naively-packed matrix by the vector."""
+    assert len(packed_matrix) == len(vector)
+
+    n = len(packed_matrix)
+    row_products = []
+    for i in range(n):
+        row_products.append(packed_matrix[i] * vector)
+
+    # Each row_product needs to be sum-reduced
+    reduced_row_products = []
+    for row in row_products:
+        reduced_row_products = rotate_and_sum(row)
+
+    # Now we need to "select" the i-th entry of each reduced_row_product and
+    # sum the extracted values together.
+
+    extracted = []
+    for i, row in enumerate(reduced_row_products):
+        mask = [0] * n
+        mask[i] = 1
+        extracted.append(row * Ciphertext(mask))
+
+    # Sum the masked values together
+    result = extracted[0]
+    for i in range(1, n):
+        result += extracted[i]
+
+    return result
