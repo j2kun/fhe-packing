fbfbf8ea2aa7e055d92309eae866222a09f9dd9f
Jeremy Kun <j2kun@users.noreply.github.com>
2024-08-18 23:19:59 -0700

    implement squat diagonal packing

--- halevi_shoup.py
+++ halevi_shoup.py
@@ -1,7 +1,10 @@
 """Halevi-Shoup matrix packing technique."""
 
+from math import log2
+
 from computational_model import Ciphertext
 from computational_model import rotate_and_sum
+from computational_model import is_power_of_two
 
 
 def pack_naive(matrix: list[list[int]]) -> list[Ciphertext]:
@@ -75,3 +78,53 @@ def matrix_vector_multiply(
 
     return result
 
+
+def pack_squat(matrix: list[list[int]]) -> list[Ciphertext]:
+    """Pack the matrix into a list of ciphertexts via
+    Juvekar-Vaikuntanathan-Chandrakasan squat diagonal packing.
+    """
+    n, m = len(matrix), len(matrix[0])
+    assert n < m
+    assert is_power_of_two(n)
+    assert is_power_of_two(m)
+
+    ciphertexts = [[None] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            # wraps around the bottom of the matrix as well as the right.
+            ciphertexts[i][j] = matrix[j % n][(i + j) % m]
+
+    return [Ciphertext(ciphertexts[i]) for i in range(n)]
+
+
+def matrix_vector_multiply_squat(
+    packed_matrix: list[Ciphertext], vector: Ciphertext
+) -> Ciphertext:
+    """Multiply the squat-diagonal-packed matrix by the vector."""
+    n, m = len(packed_matrix), len(packed_matrix[0])
+    assert m == len(vector)
+    assert n < m
+
+    row_products = []
+    for i in range(n):
+        row_products.append(packed_matrix[i] * vector.rotate(-i))
+
+    # Sum the results together
+    partial_sums = row_products[0]
+    for i in range(1, n):
+        partial_sums += row_products[i]
+
+    # Reduce the result to combine partial sums
+    result = partial_sums
+    num_shifts = int(log2(m) - log2(n))
+    shift = m // 2
+    for _ in range(num_shifts):
+        result += result.rotate(shift)
+        shift //= 2
+
+    # Mask out the first n entries
+    mask = [0] * m
+    for i in range(n):
+        mask[i] = 1
+
+    return result * Ciphertext(mask)
--- halevi_shoup_test.py
+++ halevi_shoup_test.py
@@ -1,12 +1,53 @@
 import pytest
+from hypothesis import given
+from hypothesis.strategies import composite, integers, lists
 
 from computational_model import Ciphertext
 from halevi_shoup import (
     pack,
     pack_naive,
+    pack_squat,
     matrix_vector_multiply_naive,
     matrix_vector_multiply,
+    matrix_vector_multiply_squat,
 )
+
+
+@composite
+def random_matrix(draw, shape=(4, 4)):
+    """Generate a matrix of a given shape."""
+    values = integers(min_value=-100, max_value=100)
+    matrix_row_count, matrix_col_count = shape
+    matrix = draw(
+        lists(
+            lists(values, min_size=matrix_col_count, max_size=matrix_col_count),
+            min_size=matrix_row_count,
+            max_size=matrix_row_count,
+        ),
+    )
+    return matrix
+
+
+@composite
+def random_vector(draw, dim=4):
+    """Generate a vector of a given shape."""
+    values = integers(min_value=-100, max_value=100)
+    matrix = draw(lists(values, min_size=dim, max_size=dim))
+    return matrix
+
+
+def run_test(matrix, vector, pack_fn, mul_fn):
+    packed_matrix = pack_fn(matrix)
+    result = mul_fn(packed_matrix, Ciphertext(vector))
+
+    expected = [0] * len(matrix)
+    for i, row in enumerate(matrix):
+        for j, val in enumerate(row):
+            expected[i] += val * vector[j]
+
+    # only relevant for squat diagonal packing
+    expected = expected + [0] * (len(matrix[0]) - len(expected))
+    assert result.data == expected, result.data
 
 
 @pytest.mark.parametrize(
@@ -14,9 +55,13 @@ from halevi_shoup import (
     [(pack_naive, matrix_vector_multiply_naive), (pack, matrix_vector_multiply)],
 )
 def test_matmul(pack_fn, mul_fn):
-    # each output entry ends up being 2*row[2] - 1
     matrix = [[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8], [6, 7, 8, 9]]
-    vector = Ciphertext([1, -1, 2, 0])
-    packed_matrix = pack_fn(matrix)
-    result = mul_fn(packed_matrix, vector)
-    assert result == Ciphertext([5, 9, 13, 15]), result.data
+    vector = [1, -1, 2, 0]
+    run_test(matrix, vector, pack_fn, mul_fn)
+
+
+for n in [2, 4, 8, 16]:
+
+    @given(random_matrix(shape=(n, 2 * n)), random_vector(dim=2 * n))
+    def test_matmul_squat(matrix, vector):
+        run_test(matrix, vector, pack_squat, matrix_vector_multiply_squat)
