c4a19203840b35d4834dc3cdbdfc37eebf0896cc
Jeremy Kun <j2kun@users.noreply.github.com>
2024-12-19 14:49:26 -0800

    finish affine map test impl

--- permutation_layout.py
+++ permutation_layout.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 import itertools
 
@@ -31,41 +32,29 @@ class PermutationEntry:
         return f"{self.codomain_index} -> {self.domain_index}"
 
 
-@dataclass(frozen=True, order=True)
-class GapEntry:
-    """A permutation entry marked as unused."""
-
-    index: tuple[int]
-
-    def __str__(self):
-        return f"({self.index} -> G"
-
-
 class Layout:
 
-    def __init__(self, domain_shape, codomain_shape, entries, reverse=False):
+    def __init__(self, domain_shape, codomain_shape, entries, reversed=False):
         self.domain_shape = domain_shape
         self.codomain_shape = codomain_shape
+        self.reversed = reversed
 
         def sort_key(x):
-            return x.codomain_index if reverse else x.domain_index
+            return x.codomain_index if self.reversed else x.domain_index
 
         self.entries = list(sorted(entries, key=sort_key))
-        self.verify()
+        # self.verify()
 
     def verify(self):
         for entry in self.entries:
             for index, size in zip(entry.domain_index, self.domain_shape):
-                assert index < size
-
-            if isinstance(entry, GapEntry):
-                continue
+                assert index < size, f"{index=} but {size=}"
 
             for index, size in zip(entry.codomain_index, self.codomain_shape):
-                assert index < size
+                assert index < size, f"{index=} but {size=}"
 
     def __str__(self):
-        if reversed:
+        if self.reversed:
             s = f"Layout[{self.codomain_shape} -> {self.domain_shape}](\n"
             indent = "  "
             s += "\n".join([indent + entry.reversed_str() for entry in self.entries])
@@ -79,31 +68,25 @@ class Layout:
         return s
 
 
+def generate_iteration_space(dims):
+    space = itertools.product(*[range(dim_size) for dim_size in dims])
+    for indices in space:
+        yield indices
+
+
 def row_major_layout(data_shape, ciphertext_shape):
     num_data, data_size = data_shape
     num_ciphertexts, ciphertext_size = ciphertext_shape
 
-    entries = []
-    domain_entries = []
-    codomain_entries = []
-    for ciphertext_index in range(num_ciphertexts):
-        for slot_index in range(ciphertext_size):
-            domain_entries.append((ciphertext_index, slot_index))
-
-    for data_index in range(num_data):
-        for data_slot in range(data_size):
-            codomain_entries.append((data_index, data_slot))
-
-    for c, d in itertools.zip_longest(domain_entries, codomain_entries):
-        if not d:
-            entries.append(GapEntry(*c))
-            continue
-
-        entries.append(PermutationEntry(c, d))
+    domain_entries = list(generate_iteration_space(data_shape))
+    codomain_entries = list(generate_iteration_space(ciphertext_shape))
+    entries = [
+        PermutationEntry(c, d) for (c, d) in zip(domain_entries, codomain_entries)
+    ]
 
     return Layout(
-        ciphertext_shape=ciphertext_shape,
-        data_shape=(num_data, data_size),
+        domain_shape=data_shape,
+        codomain_shape=ciphertext_shape,
         entries=entries,
     )
 
@@ -112,67 +95,103 @@ def column_major_layout(data_shape, ciphertext_shape):
     num_data, data_size = data_shape
     num_ciphertexts, ciphertext_size = ciphertext_shape
 
-    entries = []
     domain_entries = []
-    codomain_entries = []
-    for ciphertext_index in range(num_ciphertexts):
-        for slot_index in range(ciphertext_size):
-            domain_entries.append((ciphertext_index, slot_index))
-
     for data_slot in range(data_size):
         for data_index in range(num_data):
-            codomain_entries.append((data_index, data_slot))
+            domain_entries.append((data_index, data_slot))
 
-    for c, d in itertools.zip_longest(domain_entries, codomain_entries):
-        if not d:
-            entries.append(GapEntry(*c))
-            continue
-
-        entries.append(PermutationEntry(c, d))
+    codomain_entries = list(generate_iteration_space(ciphertext_shape))
+    entries = [
+        PermutationEntry(c, d) for (c, d) in zip(domain_entries, codomain_entries)
+    ]
 
     return Layout(
-        ciphertext_shape=cipertext_shape,
-        data_shape=(num_data, data_size),
+        domain_shape=data_shape,
+        codomain_shape=ciphertext_shape,
         entries=entries,
     )
 
 
 # Following MLIR's AffineExpr, we could also have Mod, FloorDiv, CeilDiv, but
 # this suffices for demonstration.
-class AffineExpr:
+class AffineExpr(ABC):
 
     def __init__(self, kind: str):
         self.kind = kind
+
+    @abstractmethod
+    def apply(self, env): ...
 
 
 class Constant(AffineExpr):
 
     def __init__(self, value: int):
-        super().__init__(self, "const")
+        super().__init__("const")
         self.value = value
+
+    def apply(self, env):
+        return self.value
 
 
 class DimId(AffineExpr):
 
     def __init__(self, dim_id: str):
-        super().__init__(self, "dim_id")
+        super().__init__("dim_id")
         self.dim_id = dim_id
+
+    def apply(self, env):
+        return env[self.dim_id]
 
 
 class Add(AffineExpr):
 
     def __init__(self, lhs: AffineExpr, rhs: AffineExpr):
-        super().__init__(self, "add")
+        super().__init__("add")
+        self.lhs = Constant(lhs) if isinstance(lhs, int) else lhs
+        self.rhs = Constant(rhs) if isinstance(rhs, int) else rhs
+
+    def apply(self, env):
+        return self.lhs.apply(env) + self.rhs.apply(env)
+
+
+class Mod(AffineExpr):
+
+    def __init__(self, lhs: AffineExpr, rhs: AffineExpr):
+        super().__init__("mod")
         self.lhs = lhs
-        self.rhs = rhs
+        self.rhs = Constant(rhs) if isinstance(rhs, int) else rhs
+        assert isinstance(self.rhs, Constant), "Affine mod op must have rhs constant!"
+
+    def apply(self, env):
+        return self.lhs.apply(env) % self.rhs.apply(env)
+
+
+class FloorDiv(AffineExpr):
+
+    def __init__(self, lhs: AffineExpr, rhs: AffineExpr):
+        super().__init__("floordiv")
+        self.lhs = lhs
+        self.rhs = Constant(rhs) if isinstance(rhs, int) else rhs
+        assert isinstance(
+            self.rhs, Constant
+        ), "Affine floordiv op must have rhs constant!"
+
+    def apply(self, env):
+        return self.lhs.apply(env) // self.rhs.apply(env)
 
 
 class Mul(AffineExpr):
 
     def __init__(self, lhs: AffineExpr, rhs: AffineExpr):
-        super().__init__(self, "mul")
-        self.lhs = lhs
-        self.rhs = rhs
+        super().__init__("mul")
+        self.lhs = Constant(lhs) if isinstance(lhs, int) else lhs
+        self.rhs = Constant(rhs) if isinstance(rhs, int) else rhs
+        assert isinstance(self.lhs, Constant) or isinstance(
+            self.rhs, Constant
+        ), "Affine mul op must have one operand constant!"
+
+    def apply(self, env):
+        return self.lhs.apply(env) * self.rhs.apply(env)
 
 
 class AffineMap:
@@ -180,35 +199,29 @@ class AffineMap:
         self.dims = dims
         self.exprs = exprs
 
+    def apply(self, env):
+        for dim in self.dims:
+            assert dim in env, f"Missing {dim} in env: {env}"
+        return tuple(expr.apply(env) for expr in self.exprs)
+
 
 def from_affine_map(affine_map: AffineMap, data_shape, ciphertext_shape):
-    # The domain is the data iteration space, and the codomain
-    # is the ciphertext iteration space
-    data_iteration_space = itertools.product(
-        [range(dim_size) for dim_size in data_shape]
-    )
-
     entries = []
-    domain_entries = []
-    codomain_entries = []
-    for indices in domain_iteration_space:
-        domain_entries.append(indices)
 
-    for data_slot in range(data_size):
-        for data_index in range(num_data):
-            codomain_entries.append((data_index, data_slot))
+    assert len(data_shape) == len(
+        affine_map.dims
+    ), f"Invalid shapes: {data_shape=} vs {len(affine_map.dims)=}"
 
-    for c, d in itertools.zip_longest(domain_entries, codomain_entries):
-        if not d:
-            entries.append(GapEntry(*c))
-            continue
+    assert len(ciphertext_shape) == len(
+        affine_map.exprs
+    ), f"Invalid shapes: {ciphertext_shape=} vs {len(affine_map.exprs)=}"
 
-        ci, cs = c
-        di, ds = d
-        entries.append(PermutationEntry(ci, cs, di, ds))
+    for c in generate_iteration_space(data_shape):
+        env = dict(zip(affine_map.dims, c))
+        entries.append(PermutationEntry(c, affine_map.apply(env)))
 
     return Layout(
-        ciphertext_shape=cipertext_shape,
-        data_shape=(num_data, data_size),
+        domain_shape=data_shape,
+        codomain_shape=ciphertext_shape,
         entries=entries,
     )
--- permutation_layout_test.py
+++ permutation_layout_test.py
@@ -3,6 +3,8 @@ from permutation_layout import AffineExpr
 from permutation_layout import AffineMap
 from permutation_layout import Constant
 from permutation_layout import DimId
+from permutation_layout import FloorDiv
+from permutation_layout import Mod
 from permutation_layout import Mul
 from permutation_layout import column_major_layout
 from permutation_layout import from_affine_map
@@ -12,45 +14,35 @@ from permutation_layout import row_major_layout
 def test_single_ciphertext_row_major_layout():
     ciphertext_shape = (1, 32)
     data_shape = (4, 4)
-    expected = """Layout[(1, 32) -> (4, 4)](
+    expected = """Layout[(4, 4) -> (1, 32)](
   (0, 0) -> (0, 0)
   (0, 1) -> (0, 1)
   (0, 2) -> (0, 2)
   (0, 3) -> (0, 3)
-  (0, 4) -> (1, 0)
-  (0, 5) -> (1, 1)
-  (0, 6) -> (1, 2)
-  (0, 7) -> (1, 3)
-  (0, 8) -> (2, 0)
-  (0, 9) -> (2, 1)
-  (0, 10) -> (2, 2)
-  (0, 11) -> (2, 3)
-  (0, 12) -> (3, 0)
-  (0, 13) -> (3, 1)
-  (0, 14) -> (3, 2)
-  (0, 15) -> (3, 3)
-  (0, 16) -> G
-  (0, 17) -> G
-  (0, 18) -> G
-  (0, 19) -> G
-  (0, 20) -> G
-  (0, 21) -> G
-  (0, 22) -> G
-  (0, 23) -> G
-  (0, 24) -> G
-  (0, 25) -> G
-  (0, 26) -> G
-  (0, 27) -> G
-  (0, 28) -> G
-  (0, 29) -> G
-  (0, 30) -> G
-  (0, 31) -> G)"""
+  (1, 0) -> (0, 4)
+  (1, 1) -> (0, 5)
+  (1, 2) -> (0, 6)
+  (1, 3) -> (0, 7)
+  (2, 0) -> (0, 8)
+  (2, 1) -> (0, 9)
+  (2, 2) -> (0, 10)
+  (2, 3) -> (0, 11)
+  (3, 0) -> (0, 12)
+  (3, 1) -> (0, 13)
+  (3, 2) -> (0, 14)
+  (3, 3) -> (0, 15))"""
     layout = row_major_layout(data_shape, ciphertext_shape)
     assert str(layout) == expected
 
     affine_map = AffineMap(
-        dims=["d0", "d1"],
-        exprs=[DimId(dim_id="d0"), DimId(dim_id="d1")],
+        dims=["row", "col"],
+        exprs=[
+            Constant(0),
+            Add(
+                Mul(data_shape[1], DimId(dim_id="row")),
+                DimId(dim_id="col"),
+            ),
+        ],
     )
     layout = from_affine_map(affine_map, data_shape, ciphertext_shape)
     assert str(layout) == expected
@@ -59,119 +51,138 @@ def test_single_ciphertext_row_major_layout():
 def test_multi_ciphertext_row_major_layout():
     ciphertext_shape = (2, 16)
     data_shape = (4, 6)
-    expected = """Layout[(2, 16) -> (4, 6)](
+    expected = """Layout[(4, 6) -> (2, 16)](
   (0, 0) -> (0, 0)
   (0, 1) -> (0, 1)
   (0, 2) -> (0, 2)
   (0, 3) -> (0, 3)
   (0, 4) -> (0, 4)
   (0, 5) -> (0, 5)
-  (0, 6) -> (1, 0)
-  (0, 7) -> (1, 1)
-  (0, 8) -> (1, 2)
-  (0, 9) -> (1, 3)
-  (0, 10) -> (1, 4)
-  (0, 11) -> (1, 5)
-  (0, 12) -> (2, 0)
-  (0, 13) -> (2, 1)
-  (0, 14) -> (2, 2)
-  (0, 15) -> (2, 3)
-  (1, 0) -> (2, 4)
-  (1, 1) -> (2, 5)
-  (1, 2) -> (3, 0)
-  (1, 3) -> (3, 1)
-  (1, 4) -> (3, 2)
-  (1, 5) -> (3, 3)
-  (1, 6) -> (3, 4)
-  (1, 7) -> (3, 5)
-  (1, 8) -> G
-  (1, 9) -> G
-  (1, 10) -> G
-  (1, 11) -> G
-  (1, 12) -> G
-  (1, 13) -> G
-  (1, 14) -> G
-  (1, 15) -> G)"""
+  (1, 0) -> (0, 6)
+  (1, 1) -> (0, 7)
+  (1, 2) -> (0, 8)
+  (1, 3) -> (0, 9)
+  (1, 4) -> (0, 10)
+  (1, 5) -> (0, 11)
+  (2, 0) -> (0, 12)
+  (2, 1) -> (0, 13)
+  (2, 2) -> (0, 14)
+  (2, 3) -> (0, 15)
+  (2, 4) -> (1, 0)
+  (2, 5) -> (1, 1)
+  (3, 0) -> (1, 2)
+  (3, 1) -> (1, 3)
+  (3, 2) -> (1, 4)
+  (3, 3) -> (1, 5)
+  (3, 4) -> (1, 6)
+  (3, 5) -> (1, 7))"""
 
     layout = row_major_layout(data_shape, ciphertext_shape)
+    assert str(layout) == expected
+
+    index = Add(
+        Mul(
+            Constant(data_shape[1]),
+            DimId(dim_id="row"),
+        ),
+        DimId(dim_id="col"),
+    )
+    affine_map = AffineMap(
+        dims=["row", "col"],
+        exprs=[
+            FloorDiv(index, ciphertext_shape[1]),
+            Mod(index, ciphertext_shape[1]),
+        ],
+    )
+    layout = from_affine_map(affine_map, data_shape, ciphertext_shape)
     assert str(layout) == expected
 
 
 def test_single_ciphertext_column_major_layout():
     ciphertext_shape = (1, 32)
     data_shape = (4, 4)
-    expected = """Layout[(1, 32) -> (4, 4)](
+    expected = """Layout[(4, 4) -> (1, 32)](
   (0, 0) -> (0, 0)
-  (0, 1) -> (1, 0)
-  (0, 2) -> (2, 0)
-  (0, 3) -> (3, 0)
-  (0, 4) -> (0, 1)
-  (0, 5) -> (1, 1)
-  (0, 6) -> (2, 1)
-  (0, 7) -> (3, 1)
-  (0, 8) -> (0, 2)
-  (0, 9) -> (1, 2)
-  (0, 10) -> (2, 2)
-  (0, 11) -> (3, 2)
-  (0, 12) -> (0, 3)
-  (0, 13) -> (1, 3)
-  (0, 14) -> (2, 3)
-  (0, 15) -> (3, 3)
-  (0, 16) -> G
-  (0, 17) -> G
-  (0, 18) -> G
-  (0, 19) -> G
-  (0, 20) -> G
-  (0, 21) -> G
-  (0, 22) -> G
-  (0, 23) -> G
-  (0, 24) -> G
-  (0, 25) -> G
-  (0, 26) -> G
-  (0, 27) -> G
-  (0, 28) -> G
-  (0, 29) -> G
-  (0, 30) -> G
-  (0, 31) -> G)"""
+  (0, 1) -> (0, 4)
+  (0, 2) -> (0, 8)
+  (0, 3) -> (0, 12)
+  (1, 0) -> (0, 1)
+  (1, 1) -> (0, 5)
+  (1, 2) -> (0, 9)
+  (1, 3) -> (0, 13)
+  (2, 0) -> (0, 2)
+  (2, 1) -> (0, 6)
+  (2, 2) -> (0, 10)
+  (2, 3) -> (0, 14)
+  (3, 0) -> (0, 3)
+  (3, 1) -> (0, 7)
+  (3, 2) -> (0, 11)
+  (3, 3) -> (0, 15))"""
     layout = column_major_layout(data_shape, ciphertext_shape)
+    assert str(layout) == expected
+
+    index = Add(
+        Mul(
+            Constant(data_shape[0]),
+            DimId(dim_id="col"),
+        ),
+        DimId(dim_id="row"),
+    )
+    affine_map = AffineMap(
+        dims=["row", "col"],
+        exprs=[
+            Constant(0),
+            index,
+        ],
+    )
+    layout = from_affine_map(affine_map, data_shape, ciphertext_shape)
     assert str(layout) == expected
 
 
 def test_multi_ciphertext_column_major_layout():
     ciphertext_shape = (2, 16)
     data_shape = (4, 6)
-    expected = """Layout[(2, 16) -> (4, 6)](
+    expected = """Layout[(4, 6) -> (2, 16)](
   (0, 0) -> (0, 0)
-  (0, 1) -> (1, 0)
-  (0, 2) -> (2, 0)
-  (0, 3) -> (3, 0)
-  (0, 4) -> (0, 1)
-  (0, 5) -> (1, 1)
-  (0, 6) -> (2, 1)
-  (0, 7) -> (3, 1)
-  (0, 8) -> (0, 2)
-  (0, 9) -> (1, 2)
-  (0, 10) -> (2, 2)
-  (0, 11) -> (3, 2)
-  (0, 12) -> (0, 3)
-  (0, 13) -> (1, 3)
-  (0, 14) -> (2, 3)
-  (0, 15) -> (3, 3)
-  (1, 0) -> (0, 4)
-  (1, 1) -> (1, 4)
-  (1, 2) -> (2, 4)
-  (1, 3) -> (3, 4)
-  (1, 4) -> (0, 5)
+  (0, 1) -> (0, 4)
+  (0, 2) -> (0, 8)
+  (0, 3) -> (0, 12)
+  (0, 4) -> (1, 0)
+  (0, 5) -> (1, 4)
+  (1, 0) -> (0, 1)
+  (1, 1) -> (0, 5)
+  (1, 2) -> (0, 9)
+  (1, 3) -> (0, 13)
+  (1, 4) -> (1, 1)
   (1, 5) -> (1, 5)
-  (1, 6) -> (2, 5)
-  (1, 7) -> (3, 5)
-  (1, 8) -> G
-  (1, 9) -> G
-  (1, 10) -> G
-  (1, 11) -> G
-  (1, 12) -> G
-  (1, 13) -> G
-  (1, 14) -> G
-  (1, 15) -> G)"""
+  (2, 0) -> (0, 2)
+  (2, 1) -> (0, 6)
+  (2, 2) -> (0, 10)
+  (2, 3) -> (0, 14)
+  (2, 4) -> (1, 2)
+  (2, 5) -> (1, 6)
+  (3, 0) -> (0, 3)
+  (3, 1) -> (0, 7)
+  (3, 2) -> (0, 11)
+  (3, 3) -> (0, 15)
+  (3, 4) -> (1, 3)
+  (3, 5) -> (1, 7))"""
     layout = column_major_layout(data_shape, ciphertext_shape)
+    assert str(layout) == expected
+
+    index = Add(
+        Mul(
+            Constant(data_shape[0]),
+            DimId(dim_id="col"),
+        ),
+        DimId(dim_id="row"),
+    )
+    affine_map = AffineMap(
+        dims=["row", "col"],
+        exprs=[
+            FloorDiv(index, ciphertext_shape[1]),
+            Mod(index, ciphertext_shape[1]),
+        ],
+    )
+    layout = from_affine_map(affine_map, data_shape, ciphertext_shape)
     assert str(layout) == expected
